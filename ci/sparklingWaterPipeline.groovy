e65ea96316f2b889ccb11a4272e34e3b31f4c6a7
Jakub Háva <jakub.hava@gmail.com>
2020-06-12 12:52:05 +0200

    debug r tests

--- ci/sparklingWaterPipeline.groovy
+++ ci/sparklingWaterPipeline.groovy
@@ -306,6 +306,7 @@ def rUnitTests() {
                          ${getGradleCommand(config)} :sparkling-water-r:test -x check -PbackendMode=${config.backendMode}
                          """
                 } finally {
+                    sh "cat r/build/rsparkling.Rcheck/00check.log"
                     arch '**/build/*tests.log,**/*.log, **/out.*, **/stdout, **/stderr, **/build/**/*log*, **/build/reports/'
                 }
             }
