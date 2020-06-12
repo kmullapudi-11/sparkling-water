cada3748b93e41ca0986456ea15a35909ce02cfb
Jakub Háva <jakub.hava@gmail.com>
2020-06-12 14:01:06 +0200

    log

--- ci/sparklingWaterPipeline.groovy
+++ ci/sparklingWaterPipeline.groovy
@@ -306,7 +306,7 @@ def rUnitTests() {
                          ${getGradleCommand(config)} :sparkling-water-r:test -x check -PbackendMode=${config.backendMode}
                          """
                 } finally {
-                    sh "cat r/build/rsparkling.Rcheck/tests/testthat.Rout"
+                    sh "cat r/build/rsparkling.Rcheck/tests/testthat.Rout.fail"
                     arch '**/build/*tests.log,**/*.log, **/out.*, **/stdout, **/stderr, **/build/**/*log*, **/build/reports/'
                 }
             }
